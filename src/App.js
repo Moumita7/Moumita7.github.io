1285511d5f6684a2d4fd1f6b5257b8f50f77f6d6
Moumita Das <dasmoumita.me@gmail.com>
2022-12-16 09:13:42 +0530

    project section

--- src/App.js
+++ src/App.js
@@ -32,3 +32,6 @@ function App() {
 }
 
 export default App;
+
+
+//npm run deploy
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -75,8 +75,8 @@ const ProjectCard = () => {
          </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore illum odit adipisci quasi autem perspiciatis tenetur assumenda, consectetur possimus a deserunt ea accusantium quod officiis nulla doloribus voluptatem natus facere.</p>
             <div className='p_btn'>
-            <button className='btn'><a a className='aa'  style={{ textDecoration: 'none' }} href="https://incredible-basbousa-aecbd0.netlify.app/">Check it Out!</a></button>
-        <button className='btn'><a className='aa'  style={{ textDecoration: 'none' }} href='https://github.com/Moumita7/foamy-language-9425'>Github Link</a></button>
+            <button className='btn'><a a className='aa'  style={{ textDecoration: 'none' }} href="https://calm-kataifi-ed53c8.netlify.app/">Check it Out!</a></button>
+        <button className='btn'><a className='aa'  style={{ textDecoration: 'none' }} href='https://github.com/sgrprmnk/paltry-rain-9292'>Github Link</a></button>
 
         </div>
         </div>
@@ -95,8 +95,8 @@ const ProjectCard = () => {
          </div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore illum odit adipisci quasi autem perspiciatis tenetur assumenda, consectetur possimus a deserunt ea accusantium quod officiis nulla doloribus voluptatem natus facere.</p>
             <div className='p_btn'>
-            <button className='btn'><a a className='aa'  style={{ textDecoration: 'none' }} href="https://incredible-basbousa-aecbd0.netlify.app/">Check it Out!</a></button>
-        <button className='btn'><a className='aa'  style={{ textDecoration: 'none' }} href='https://github.com/Moumita7/foamy-language-9425'>Github Link</a></button>
+            <button className='btn'><a a className='aa'  style={{ textDecoration: 'none' }} href="https://voluble-twilight-07e9bc.netlify.app/">Check it Out!</a></button>
+        <button className='btn'><a className='aa'  style={{ textDecoration: 'none' }} href='https://github.com/Moumita7/Netflix-clone'>Github Link</a></button>
         </div>
         </div>
     </div>
